9fe3ce63c8ac38ab950bd45d0992e639fbcdec61
ZGhanbari <z.ghanbari14@gmail.com>
2020-12-16 22:50:04 +0330

    Updates

---
+++ precache-manifest.c459e4ec2d7830a13510be154ed080ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "714a2cdc6dc41505df4b",
+    "url": "/todo/static/css/main.78b3ae4b.chunk.css"
+  },
+  {
+    "revision": "714a2cdc6dc41505df4b",
+    "url": "/todo/static/js/main.714a2cdc.chunk.js"
+  },
+  {
+    "revision": "94b4d4d0fc772f874bc8",
+    "url": "/todo/static/js/1.94b4d4d0.chunk.js"
+  },
+  {
+    "revision": "8a695b30138abc35248a",
+    "url": "/todo/static/js/runtime~main.8a695b30.js"
+  },
+  {
+    "revision": "d0a7897f0615cc9b7e79595ecebf8b53",
+    "url": "/todo/index.html"
+  }
+];
